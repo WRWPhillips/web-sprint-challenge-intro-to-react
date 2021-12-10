705883e9fbaf90d9d09b3f2fe1a0ab2d45f7ada6
Will Phillips <42WPhillips42@gmail.com>
2021-12-09 21:29:42 -0600

    MVP completed except for styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import styled from 'styled-components';
+import CharacterMaker from './components/Character';
 
+// call character component Character
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -8,10 +12,23 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+  const [character, setCharacter] = useState([]);
+  let url = 'https://swapi.dev/api/people'
+  useEffect(() =>{
+      axios.get(url).then((response)=>{
+          console.log(response);
+          setCharacter(response.data);
+          console.log(character);
+      })
+  }, []);
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {
+        character.map((e) => {
+          return CharacterMaker(e);
+        })
+      }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,13 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 // Write your Character component here
+
+export default function CharacterMaker(props) {
+    return (    
+        <div className='characterCard'>
+            <h3 class='characterName'>{props.name}:</h3>
+            <p class='characterHeight'>Height: {props.height}</p>
+            <p class='characterGender'>Sex: {props.gender}</p>
+        </div>
+    )
+}
